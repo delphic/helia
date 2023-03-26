b84f57c5c93a434354be17af35b8f89e8ad4f268
Harry Jones <delphic.uk@gmail.com>
2023-03-26 17:31:43 +0100

    Made default log level explicit

--- helia_internal/src/lib.rs
+++ helia_internal/src/lib.rs
@@ -265,7 +265,7 @@ pub async fn run(mut game: Box<dyn Game>) {
             std::panic::set_hook(Box::new(console_error_panic_hook::hook));
             console_log::init_with_level(log::Level::Warn).expect("Couldn't initialize logger");
         } else {
-            env_logger::init();
+            env_logger::builder().filter_level(log::LevelFilter::Warn).init();
         }
     }
 
