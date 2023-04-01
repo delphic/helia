bf14ca51aa6fcfe0f56134142134a077d6bb519f
Harry Jones <delphic.uk@gmail.com>
2023-04-01 20:37:37 +0100

    Set default log level to info

--- helia_internal/src/lib.rs
+++ helia_internal/src/lib.rs
@@ -227,9 +227,9 @@ pub async fn run(mut game: Box<dyn Game>) {
     cfg_if::cfg_if! {
         if #[cfg(target_arch = "wasm32")] {
             std::panic::set_hook(Box::new(console_error_panic_hook::hook));
-            console_log::init_with_level(log::Level::Warn).expect("Couldn't initialize logger");
+            console_log::init_with_level(log::Level::Info).expect("Couldn't initialize logger");
         } else {
-            env_logger::builder().filter_level(log::LevelFilter::Warn).init();
+            env_logger::builder().filter_level(log::LevelFilter::Info).init();
         }
     }
 
